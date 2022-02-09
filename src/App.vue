823809b1f12d22c2cf8b8fedf69b77b2ba47bdb3
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-02-08 21:52:55 -0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Navbar />
+  <Main />
+  <footer></footer>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Main,
+    Footer,
+  },
+};
+</script>
